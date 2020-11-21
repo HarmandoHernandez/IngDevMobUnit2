a06fde7f999f84fbf45107b88b53bdfe5b8af0fc
HarmandoHernandez <39201744+HarmandoHernandez@users.noreply.github.com>
2020-11-20 21:54:31 -0600

    Avance Room Note

--- IntrumentoUnit2/app/src/main/java/com/utng/intrumentounit2/NoteActivity.kt
+++ IntrumentoUnit2/app/src/main/java/com/utng/intrumentounit2/NoteActivity.kt
@@ -1,8 +1,10 @@
 package com.utng.intrumentounit2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_note.*
+
 
 /** Valida **/
 class NoteActivity : AppCompatActivity() {
@@ -10,7 +12,7 @@ class NoteActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note)
 
-        // supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        //supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val topic = intent.getSerializableExtra("note") as NoteModel
 
@@ -19,4 +21,35 @@ class NoteActivity : AppCompatActivity() {
         textContent.text = topic.content
 
     }
+
+    /**
+     * Guarda la nota si no existe (mNota es null), o la actualiza si existe.
+    private fun guardar() {
+        val textoNota: String = mTextoNota.getText().toString()
+        if (textoNota != "") {
+            if (mNota == null) {
+                mNota = Nota()
+                mNota.setMensaje(textoNota)
+                mNotaLab.addNota(mNota)
+                Toast.makeText(
+                    this, getString(R.string.nota_creada),
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                mNota.setMensaje(textoNota)
+                mNotaLab.updateNota(mNota)
+                Toast.makeText(
+                    this, getString(R.string.nota_actualizada),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        } else {
+            Toast.makeText(
+                this, getString(R.string.crea_nota_primero),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+     */
+
 }
